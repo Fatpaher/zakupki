a9a0f2bcd9525f9082d48cdbb8ca5df5b56fd8e7
xuliaP <paukov2967@gmail.com>
2016-05-24 22:19:56 +0300

    add validation comment

---
+++ spec/controllers/add_comment_to_purchase_spec.rb
@@ -0,0 +1,11 @@
+require "rails_helper"
+
+describe "User visit purchase show page" do
+  it "user see comments" 
+  context "unregistered" do
+  	it "can't leave comments" 
+  end
+  context "sign-in" do 
+  	it "can leave comments" 
+  end
+end
