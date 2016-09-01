9c67bc8428fa0a12d9c48e123284fb7ffb695983
Paul Pavlovsky <fatpaher@gmail.com>
2016-09-01 13:10:53 +0300

    add webkit debug

--- spec/acceptance_helper.rb
+++ spec/acceptance_helper.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.configure do |config|
-  Capybara.javascript_driver = :webkit
+  Capybara.javascript_driver = :webkit_debug
   Capybara.default_max_wait_time = 10
   Capybara.ignore_hidden_elements = false
   config.use_transactional_fixtures = false
